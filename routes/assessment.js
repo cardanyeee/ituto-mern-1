71bd39e2a3218f1554411e98ebbf5cf792371668
sayrsalad <77481152+sayrsalad@users.noreply.github.com>
2022-03-23 13:06:16 +0800

    Answer Assessment

--- routes/assessment.js
+++ routes/assessment.js
@@ -6,12 +6,14 @@ const {
     findTutorAssessments,
     findTuteeAssessments,
     allExam,
-    selectedExam
+    selectedExam,
+    answerAssessment
 } = require('../controllers/assessment');
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 router.route("/assessment/create").post(isAuthenticatedUser, create);
+router.route("/assessment/answer/:id").post(isAuthenticatedUser, answerAssessment);
 router.route("/assessment/tutor").get(isAuthenticatedUser, findTutorAssessments);
 router.route("/assessment/tutee").get(isAuthenticatedUser, findTuteeAssessments);
 router.route("/assessments").get(allExam);
